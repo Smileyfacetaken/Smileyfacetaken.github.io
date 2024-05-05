caab1628f5ec900fe50c48aa8d732eead7d1a34f
Smileyfacetaken <smileyfacetaken@gmail.com>
2024-05-04 21:41:34 -0500

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,18 +1,24 @@
-function playerChoice(choice) {
-    var choices = ['rock', 'paper', 'scissors'];
-    var computerChoice = choices[Math.floor(Math.random() * 3)];
-    
-    var result = '';
-    
-    if (choice === computerChoice) {
-        result = "It's a tie!";
-    } else if ((choice === 'rock' && computerChoice === 'scissors') ||
-               (choice === 'paper' && computerChoice === 'rock') ||
-               (choice === 'scissors' && computerChoice === 'paper')) {
-        result = "You win!";
-    } else {
-        result = "You lose!";
-    }
-    
-    document.getElementById('result').innerHTML = `You chose ${choice}. Computer chose ${computerChoice}. ${result}`;
-}
+document.addEventListener('DOMContentLoaded', function() {
+    var buttons = document.querySelectorAll('.choices button');
+    var resultDiv = document.getElementById('result');
+
+    buttons.forEach(function(button) {
+        button.addEventListener('click', function() {
+            var playerChoice = this.id;
+            var computerChoice = ['rock', 'paper', 'scissors'][Math.floor(Math.random() * 3)];
+            var result = '';
+
+            if (playerChoice === computerChoice) {
+                result = "It's a tie!";
+            } else if ((playerChoice === 'rock' && computerChoice === 'scissors') ||
+                       (playerChoice === 'paper' && computerChoice === 'rock') ||
+                       (playerChoice === 'scissors' && computerChoice === 'paper')) {
+                result = "You win!";
+            } else {
+                result = "You lose!";
+            }
+
+            resultDiv.textContent = `You chose ${playerChoice}. Computer chose ${computerChoice}. ${result}`;
+        });
+    });
+});
